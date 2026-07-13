aff1d995436bd16afe206379c4290decd4cd5b15
Abdel Raoof <olakara@gmail.com>
2026-07-13 17:08:03 +0400

    Scaffold solution structure with Domain, WebApi, and test projects

---
+++ test/Handoff.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Handoff.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/Handoff.WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Handoff.WebApi.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
